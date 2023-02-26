1806a908212f22a0b359e77cf5acc92d7102cf7e
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-02-26 18:27:38 +0100

    acabdo

--- paralelo.py
+++ paralelo.py
@@ -19,5 +19,27 @@ def time_secuencial(urls):
     output = []
     for url in urls:
         output.append(scrape(url))
-    Tiempo = (time.time() - tiempo_inicial, "segundos")
-    print("El tiempo total en programacion secuencial es: ", Tiempo)
+    Tiempo_secuencial = (time.time() - tiempo_inicial, "segundos")
+    print("El tiempo total en programacion secuencial es: ", Tiempo_secuencial)
+
+
+def time_multiprocesamiento(urls):
+    tiempo_inicial = time.time()
+    pool = Pool(processes=4)
+    data = pool.map(scrape, urls)
+    pool.close()
+    Tiempo_multi = (time.time() - tiempo_inicial, "segundos")
+    print("El tiempo total en programacion multiprocesamiento es: ", Tiempo_multi)
+
+
+
+# ejecuto en paralelo ambos programas
+if __name__ == '__main__':
+    p1 = Process(target=time_secuencial, args=(urls,))
+    p2 = Process(target=time_multiprocesamiento, args=(urls,))
+    p1.start()
+    p1.join()
+    p2.start()
+    p2.join()
+
+    
